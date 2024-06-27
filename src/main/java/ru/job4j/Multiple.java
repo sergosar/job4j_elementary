b4c31ad40f8bedc4b472a50d00f3bd98ab67f5b6
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-06-27 11:28:42 +0300

    4.1. Разбор первой программы. [#159033 #502556]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,11 +1,11 @@
 package ru.job4j;
-
+/** пардон , лень но и так подойдёт я думаю */
 public class Multiple {
     public static void main(String[] args) {
 
         for (int i = 1; i < 10; i++) {
             System.out.println("1 * " + i + " = " + i);
-//пардон , лень но и так подойдёт я думаю
+
         }
     }
 }
