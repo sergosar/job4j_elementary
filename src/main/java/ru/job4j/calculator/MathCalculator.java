b3db6470e9eaabbe559e0cace87d1c4145ba4eea
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-06-29 13:59:47 +0300

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDivision(double first, double second) {
+        return difference(first, second)
+                + division(first, second);
+    }
+
+    public static double sumAll(double first, double second) {
+        return difference(first, second)
+                + division(first, second) + sum(first, second) + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
