714b7a629125490006368318bc2abb8eba186b85
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-06-30 13:31:16 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,18 +2,35 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println("fourTimeTwo = " + fourTimeTwo);
-        System.out.println("fiveMinusTwo = " + fiveMinusTwo);
-        System.out.println("sixDivTwo = " + sixDivTwo);
+        int resultAdd = add(1, 2);
+        System.out.println("1 + 2 = " + resultAdd);
+
+        int resultDiv = divide(6, 2);
+        System.out.println("6 / 2 = " + resultDiv);
+
+        int resultSub = subtract(5, 2);
+        System.out.println("5 - 2 = " + resultSub);
+
+        int resultMul = multiply(4, 2);
+        System.out.println("4 * 2 = " + resultMul);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
     }
 }
