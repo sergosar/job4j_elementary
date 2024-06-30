500cc6b729f7ea348a1d7b34debce9eb81f12aa8
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-06-30 12:30:31 +0300

    1.2. Входные данные в тесте [#297750 #502878]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -1,15 +1,9 @@
 package ru.job4j.condition;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
-/*   - a = 1, b = 1, c = 1, x = 1.
 
-   - a = 0, b = 1, c = 1, x = 1.
-
-   - a = 1, b = 1, c = 0, x = 1.
-
-   - a = 1, b = 1, c = 1, x = 0.
-* */
 class X2Test {
 
     @Test
@@ -18,67 +12,52 @@ class X2Test {
         int b = 0;
         int c = 0;
         int x = 2;
-
         int expected = 40;
-
         int result = X2.calc(a, b, c, x);
-
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenA1B1C1X1Then40() {
+    void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
-
         int expected = 3;
-
         int result = X2.calc(a, b, c, x);
-
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenA0B1C1X1Then40() {
+    void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-
         int expected = 2;
-
         int result = X2.calc(a, b, c, x);
-
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenA1B1C0X1Then40() {
+    void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-
         int expected = 2;
-
         int result = X2.calc(a, b, c, x);
-
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenA1B1C1X0Then40() {
+    void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
-
         int expected = 1;
-
         int result = X2.calc(a, b, c, x);
-
         assertThat(result).isEqualTo(expected);
     }
 }
