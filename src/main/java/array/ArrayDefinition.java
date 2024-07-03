a7612980f90dae7a963667e87fb88c3c26cefc49
SNMiroshnichenko <SNMiroshnichenko@vniief.ru>
2024-07-03 16:55:55 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("surnames.length = " + surnames.length);
         float[] prices = new float[40];
         System.out.println("prices.length = " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Vladimir Putin";
+        names[2] = "Vladimir Zhirinovsky";
+        names[3] = "Oleg Gazmanov";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
